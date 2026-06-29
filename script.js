272a85a3b2247ad69ff9e301f84b6eee22ee2a17
Asmit102 <asmitm750@gmail.com>
2026-06-29 07:56:00 +0530

    Changes

---
+++ script.js
@@ -0,0 +1,28 @@
+
+AOS.init({
+        offset:100,
+        duration:1000,
+    });
+
+    function resetFormAfterSubmit(e) {
+    setTimeout(() => {
+        e.target.reset();  // Clears all form fields after submission
+    }, 1000); // Delay to allow Formspree to process
+}
+//hamburger ke liye
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".menu");
+const menuIcon = document.querySelector(".menu-toggle i");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    if(navLinks.classList.contains("active")){
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+    }else{
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+    }
+});
